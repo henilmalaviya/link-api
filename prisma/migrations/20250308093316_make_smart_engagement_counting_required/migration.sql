5297b5946846bad69e3c1aca1d18a60022c096c8
Henil Malaviya <henilm.dev@gmail.com>
2025-09-17 10:46:04 +0530

    clean

--- prisma/migrations/20250308093316_make_smart_engagement_counting_required/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `smartEngagementCounting` on table `Link` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Link" ALTER COLUMN "smartEngagementCounting" SET NOT NULL;
--- prisma/migrations/20250308105227_add_enabled_flag/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Link" ADD COLUMN     "enabled" BOOLEAN NOT NULL DEFAULT true;
